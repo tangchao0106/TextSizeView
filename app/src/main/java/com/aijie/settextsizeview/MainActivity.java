073b305d48dd4d274f12edb97e86c7f474d21c96
tangchao <419704299@qq.com>
2017-08-25 17:23:10 +0800

    增加字体

--- app/src/main/java/com/aijie/settextsizeview/MainActivity.java
+++ app/src/main/java/com/aijie/settextsizeview/MainActivity.java
@@ -2,12 +2,20 @@ package com.aijie.settextsizeview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements SetTextSizeView.OnPointResultListener {
+    SetTextSizeView mSetTextSizeView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSetTextSizeView = (SetTextSizeView) findViewById(R.id.stsize);
+        mSetTextSizeView.setOnPointResultListener(this);
+    }
+
+    @Override
+    public void onPointResult(int position) {
+        Toast.makeText(this, "position==="+position, Toast.LENGTH_SHORT).show();
     }
 }
